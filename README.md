04761bb86016a206f68959227cf3436abc7b2353
shivam.singh <shivam611996@gmail.com>
2020-09-18 13:50:00 +0530

    babel-loader and gh-pages

---
+++ README.md
@@ -0,0 +1,13 @@
+Quick Start
+
+# Install dependencies
+
+npm install
+
+# Serve on localhost:8080
+
+npm start
+
+# Build for production
+
+npm run build
--- webpack.config.js
+++ webpack.config.js
@@ -2,58 +2,54 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: "./src/index.tsx",
-  target: "web",
-  mode: "development",
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js"
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "ts-loader"
-          }
-        ]
-      },
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader"
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: true
-            }
-          },
-          "css-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/i,
-        use: ["file-loader"]
-      }
+module.exports = (env, options) => {
+  const isDevMode = options.mode === "development";
+
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js"
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
+    },
+    module: {
+      rules: [{
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader", "ts-loader"]
+        },
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          loader: "source-map-loader"
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [{
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: isDevMode ? true : false // hot module replacement
+              }
+            },
+            "css-loader",
+            "sass-loader"
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/i,
+          use: ["file-loader"]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html")
+      }),
+      new MiniCssExtractPlugin({
+        filename: "./src/index.css"
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html")
-    }),
-    new MiniCssExtractPlugin({
-      filename: "./src/index.css"
-    })
-  ]
+  }
 };
